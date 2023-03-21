f807249cb6e99eaa6220b037be85b37100e2c7e4
shital-kamble <94334804+shital-kamble@users.noreply.github.com>
2023-03-22 01:05:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+console.log("Promise Methods");
+
+let promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise1");
+  }, 1000);
+});
+
+let promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // reject("Could not resolve promise 2");
+    resolve("Promise 2");
+  }, 2000);
+});
+
+let promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise 3");
+  }, 3000);
+});
+
+const myArray = [promise1, promise2, promise3];
+
+Promise.all(myArray)
+  .then((res) => {
+    console.log("ANY", res);
+  })
+  .catch((e) => console.log(e));
